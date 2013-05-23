811371907f871e1ec934678ac5749cea0f1bc012
Meagan Waller <MeaganEWaller@Gmail.com>
2013-05-23 14:38:29 -0400

    Changed Wrong wording on first test, second test is now failing

--- spec/ttt/scoring_spec.rb
+++ spec/ttt/scoring_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 module TicTacToe
   describe Scoring do
 
-    it 'knows when the board is not empty' do
+    it 'knows when the board is empty' do
       scoring = Scoring.new
       board = Board.new
       scoring.is_board_empty?(board).should == true
     end
 
+    it 'knows when the board is not empty' do
+      scoring = Scoring.new
+      board = Board.new
+      board.place_mark(1, "X")
+      scoring.is_board_empty?(board).should == false
+    end
 
   end
 end
