c46f6ff49c15856515650e531567742e088ed59c
Meagan Waller <MeaganEWaller@Gmail.com>
2013-05-23 15:32:58 -0400

    refactoring spec file

--- spec/ttt/scoring_spec.rb
+++ spec/ttt/scoring_spec.rb
@@ -1,27 +1,27 @@
 require 'spec_helper'
 module TicTacToe
   describe Scoring do
+    let(:board) { Board.new }
+    let(:scoring) { Scoring.new }
 
     it 'knows when the board is empty' do
-      scoring = Scoring.new
-      board = Board.new
       scoring.is_board_empty?(board).should == true
     end
 
     it 'knows when the board is not empty' do
-      scoring = Scoring.new
-      board = Board.new
       board.place_mark(1, "X")
       scoring.is_board_empty?(board).should == false
     end
 
     it 'knows if there is a winner' do
-      scoring = Scoring.new
-      board = Board.new
       board.place_mark(1, "X")
       board.place_mark(4, "X")
       board.place_mark(7, "X")
       scoring.winner?(board).should == true
+    end
+
+    it 'knows when there is not a winner' do
+      scoring.winner?(board).should == false
     end
 
   end
