42189e49cc5f0705f9cff8edf41633b46ef912b0
Meagan Waller <MeaganEWaller@Gmail.com>
2013-05-23 10:44:21 -0400

    slight refactoring of board spec file

--- spec/ttt/board_spec.rb
+++ spec/ttt/board_spec.rb
@@ -2,24 +2,21 @@ require 'spec_helper'
 
 module TicTacToe
   describe Board do
+    let(:board) { Board.new }
 
       it 'starts with a blank do' do
-        board = Board.new
         board.spaces.should == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       end
 
       it 'can retrieve a space' do
-        board = Board.new
         board.get(7).should == " "
       end
 
       it 'can place a mark in a space' do
-        board = Board.new
         board.place_mark(7, "X")
       end
 
       it 'can read a mark placed in a space' do
-        board = Board.new
         board.place_mark(1, "X")
         board.get(1).should == "X"
       end
